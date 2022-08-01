d8d1a59aadf1cfe602e7b8b50a1e831add453955
Luan Fortuna <luan395@gmail.com>
2022-07-31 22:29:14 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+
+// console.log("TESTANDO SCRIPT RODANDO")
+
+const input1 = document.getElementById('num1') as HTMLInputElement;
+
+const input2 = document.getElementById('num2') as HTMLInputElement;
+
+const buttonSoma = document.getElementById("buttonSomar") as HTMLElement;
+
+const buttonSubtrair = document.getElementById("buttonSubtrair") as HTMLElement;
+
+function somar(a: number, b: number): number {
+
+    return a + b;
+}
+
+buttonSoma.addEventListener('click', function () {
+
+    const resultado = somar(Number(input1.value), Number(input2.value));
+
+    console.log(resultado);
+})
+
+function subtrair(a: number, b: number): number {
+
+    return a - b;
+}
+
+buttonSubtrair.addEventListener('click', function () {
+
+    const resultado = subtrair(Number(input1.value), Number(input2.value));
+
+    console.log(resultado);
+})
